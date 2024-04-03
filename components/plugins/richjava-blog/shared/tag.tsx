4ebe213368e86d9f26aa2885a736a9ed16cce5f2
Richard Lovell <richardlovell@Richards-MacBook-Air.local>
2024-04-03 22:49:27 +1300

    Updated tag styles

--- components/plugins/richjava-blog/shared/tag.tsx
+++ components/plugins/richjava-blog/shared/tag.tsx
@@ -1,7 +1,7 @@
 export default function Tag({ data }:any) {
   if (!data) return <></>;
   return (
-    <div className="tag" key={data.name}>
+    <div className="px-2 py-1 text-center text-white no-underline transition-colors text-label-01 bg-accent dark:bg-light-03 rounded-corner-04 dark:hover:bg-light-02 hover:bg-dark-01 dark:text-dark-04" key={data.name}>
       {data.name}
     </div>
   );
